e9137274bbfa8bc5e93c21f2532813569a4ccc5b
Jacob Delgadillo <jacobdelgadillo751@gmail.com>
2022-04-21 15:03:14 -0700

    commit tree 69c9366e77e781d00f81691c05970aa13132e0b2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+function superbowlWin(win){
+   for(const record of win){
+       if(record.year === '2015'){
+           return '2015';
+       }
+   }
+}  
